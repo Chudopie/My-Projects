33714d35b45a98ef8c9d4fdafb2e04c8d19129e5
Eduardo Gonzalez <lalolu94@hotmail.com>
2024-02-16 04:06:43 -0600

    cv csharp FCCDOM

---
+++ PROJECTS/FreecodecampDOM/toppings-de-pizza/appDOM.js
@@ -0,0 +1,46 @@
+const contenedor = document.getElementById("contenedor");
+//INNERHTML STRING
+
+const titulo = document.getElementById("titulo");
+console.log(typeof titulo.innerHTML); // lo que tiene el elemento del contenedor del div en formato html y el typeof es string
+
+//OBJETO sin ningun inner devuelve objeto
+
+console.log(typeof contenedor); // lo que tiene el elemento del contenedor del div en formato html y el typeof es objeto
+
+//INNERTEXT
+console.log(typeof titulo.innerText); // lo que tiene el elemento del contenedor del div en formato html y el typeof es string
+
+//TAGNAME STRING y te devuelve el H1 o elemento
+console.log(titulo.tagName);
+
+//getelementbyclassname
+//se puede ingresar al primero de la lista ordenada, con arreglo y al nombre del id con [0].id
+const topping = document.getElementsByClassName("topping");
+console.log(topping);
+console.log(topping[0]);
+
+//.getelementbytagname se busca el elemento por el tagname o su etiqueta por ejemplo el li el elemento que es la lista ordenada
+const misToppings = document.getElementsByTagName("li");
+console.log(misToppings);
+//aqui se selecciona solo los de fondo marron por classname
+const toppingsFondoMarron = document.getElementsByClassName("fondo-marron");
+console.log(toppingsFondoMarron);
+//QUERYSELECTOR
+const ullifondonaranja = document.querySelector("ul li.fondo-naranja");
+console.log(ullifondonaranja);
+
+const nofondomarron = document.querySelector("ul li:not(.fondo-marron)");
+console.log(nofondomarron); //niega las caracteristicas que señalan al elemento que en este caso es una clase de la lista ul, que dentro tiene una li y que niega las clases .fondo-marron y selecciona las que no son .fondo-marron, por el not (en realidad solo selecciona el primer topping que es cebolla y no es marron)
+
+//QUERYSELECTOR ALL
+const toppingNaranja = document.querySelectorAll("ul li:not(.fondo-marron)");
+console.log(toppingNaranja);
+console.log(toppingNaranja[0]);
+console.log(toppingNaranja[1]);
+console.log(toppingNaranja.length);
+console.log(typeof toppingNaranja);
+
+//ASIGNAR ESTILO CON JS
+const primerTopping = document.querySelector(".topping");
+console.log(primerTopping.style);
---
+++ PROJECTS/csharp/Pokedex/Pokedex/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Pokedex
+{
+    public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+         {
+            string servidor = "localhost";
+            string bd = "hello_sql";
+            string user = "root";
+            string pass = "admin1234";
+            string datos = "null";
+
+            string cadenaConexion = "Database=" +bd + "; Data Source=" + servidor + ";User id="+ user + "; Password=" + pass + "" ;
+            MySqlConnection connectionDB = new MySqlConnection(cadenaConexion);
+            MySqlDataReader reader = null;
+            try
+            {
+                string consulta = "select * from pokeusers;";
+                MySqlCommand comando = new MySqlCommand(consulta);
+                comando.Connection = connectionDB;
+                connectionDB.Open();
+                
+
+                reader = comando.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    datos += reader.GetString(1) +":"+ reader.GetString(2) + "\n";
+                }
+
+
+
+                MessageBox.Show(datos);
+                  
+               
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connectionDB.Close();
+            }
+         }
+    }
+}
+
