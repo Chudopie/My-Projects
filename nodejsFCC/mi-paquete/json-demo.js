7957ff749bdeec5bee569534c5a26b0d4662d4e4
Chudopie <lalolu94@hotmail.com>
2024-03-10 09:47:26 -0600

    nodejs laptop

--- nodejsFCC/mi-paquete/json-demo.js
+++ nodejsFCC/mi-paquete/json-demo.js
@@ -1,12 +1,22 @@
 let infocurso = {
     "titulo": "Aprende Node.js",
     "numVistas": 45642.,
-    "temas:" [
+    "temas": [
         "JavaScript",
         "Node.js",
     ],
     "esPublico":true
 }   
+//Objeto -> Cadena de Caracteres
+
+//Cadena de caracteres en formato JSON
 let infoCursoJSON = JSON.stringify(infocurso);
+console.log(infoCursoJSON);
 console.log(typeof infoCursoJSON);
-//2:37:42
+
+
+//Cadena de Caracteres -> Objeto de JS
+let infoCursoObjeto = JSON.parse(infoCursoJSON); //parse permite convertir una cadena de caracteres a objeto
+console.log(infoCursoObjeto);
+console.log(typeof infoCursoObjeto);
+console.log(infoCursoObjeto.titulo);
