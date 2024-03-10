bb160e73717c2ce25ec38d5dad311085cd51daaf
Chudopie <lalolu94@hotmail.com>
2024-03-09 19:57:04 -0600

    sabado8pmnode

---
+++ nodejsFCC/mi-paquete/json-demo.js
@@ -0,0 +1,12 @@
+let infocurso = {
+    "titulo": "Aprende Node.js",
+    "numVistas": 45642.,
+    "temas:" [
+        "JavaScript",
+        "Node.js",
+    ],
+    "esPublico":true
+}   
+let infoCursoJSON = JSON.stringify(infocurso);
+console.log(typeof infoCursoJSON);
+//2:37:42
