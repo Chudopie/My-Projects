4bb226f741e2646313b7f9a780fcceb6839d34ae
Eduardo Gonzalez <lalolu94@hotmail.com>
2024-04-19 18:04:18 -0600

    beforebonelss

--- PROJECTS/09-Data-Structures-Operators/starter/assignments.js
+++ PROJECTS/09-Data-Structures-Operators/starter/assignments.js
@@ -222,7 +222,37 @@ const books = [
     highlighted: true,
   },
 ];
-2;
 
+//Destructuring Arrays
+// 1.1﻿
+// Destructure the books array into two variables called firstBook and secondBook.
 const [firstBook, secondBook] = books;
-console.log(firstBook, secondBook);
+
+
+// 1.2﻿
+// Destructure the books array into a variable called thirdBook. You must skip the first two books.
+const [ , , thirdBook] = books
+
+
+// 1.3﻿
+// Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+
+const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+[[,ratings],[,ratingsCount]] = ratings
+// 1.4﻿
+// Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+
+const ratingStars = [63405, 1808];
+[fiveStarRatings,oneStarRatings,threeStarRatings=0] = ratingStars
+//Destructuring Objects
+// 2.1﻿
+// Destructure the first book object from the books array into variables called title, author and ISBN.
+const {title,author,ISBN} = books[0]
+
+// 2.2﻿
+// Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
+
+const {keywords:tags} = books[0]
+
+// 2.3﻿
+// The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
--- PROJECTS/09-Data-Structures-Operators/starter/script.js
+++ PROJECTS/09-Data-Structures-Operators/starter/script.js
@@ -6,25 +6,83 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDeliverly: function({starterIndex =1,mainIndex = 0,time = '20:00',adress}){
+    console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${ this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`);
+  }
 };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+restaurant.orderDeliverly({
+  time:'22:30',
+  adress:'Via del Sole,21',
+  mainIndex:2,
+  starterIndex:2
+})
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+restaurant.orderDeliverly({
+  adress:'Via del Sole,21',
+  starterIndex:1
+})
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+//Object Destructuring
+//en los objetos no importa el orden como en los arrays
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+//Se le pueden colocar o definir otro nombre  alas variables al destructurar objetos
+const {
+  name: restaurntName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurntName, hours, tags);
+//Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating Variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+  ({ a, b } = obj);
+console.log(a, b);
+
+
+//Nested Objects
+const {fri:{open:o,close:c}}= openingHours
+console.log(o,c);
+
+//Destructuring Arrays
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
 //Switching Variables
 //hacer lo de arriba
@@ -36,15 +94,15 @@ console.log(main, secondary);
 // console.log(main, secondary);
 
 //Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 //Nested Destructuring
-const nested = [2, 4, [5, 6]];
+// const nested = [2, 4, [5, 6]];
 // const [i, , [j]] = nested;
 // console.log(i, j);
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
 //Default values
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
