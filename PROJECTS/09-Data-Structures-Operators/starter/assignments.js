71cfd64003aa2145ca937a6abcb76c1c26f38e93
Eduardo Gonzalez <lalolu94@hotmail.com>
2024-04-22 12:50:05 -0600

    cursodia1

--- PROJECTS/09-Data-Structures-Operators/starter/assignments.js
+++ PROJECTS/09-Data-Structures-Operators/starter/assignments.js
@@ -242,6 +242,8 @@ const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
 // 1.4﻿
 // Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
 
+//DESTRUCTURING OBJECTS
+
 const ratingStars = [63405, 1808];
 [fiveStarRatings,oneStarRatings,threeStarRatings=0] = ratingStars
 //Destructuring Objects
@@ -256,3 +258,80 @@ const {keywords:tags} = books[0]
 
 // 2.3﻿
 // The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
+
+const {language, programmingLanguage ='unknown'} = books[6]
+
+// 2.4﻿
+// Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({title: bookTitle, author:bookAuthor}) = books[0]
+
+// 2.5﻿
+// Each book object has a deeply nested rating property as illustrated below:
+// {
+//   title: 'Algorithms',
+//   ...
+//   thirdParty: {
+//     goodreads: {
+//       rating: 4.41,              // <-- HERE
+//       ratingsCount: 1733,
+//       reviewsCount: 63,
+//       fiveStarRatingCount: 976,
+//       oneStarRatingCount: 13
+//     }
+//   }
+// },
+
+// Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
+
+// Please do most of the work on the left side of the assignment operator: const ... = books[0];
+
+({thirdParty:{goodreads:{rating:bookRating}}}) = books[0]
+
+// 2.6﻿
+// Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
+
+// If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
+
+function printBookInfo (title,author,year = 'year unknown') {
+  console.log(`The title is ${title} by ${author}, ${year}`);
+}
+//THE SPREAD OPERATOR
+///////////////THE SPREAD OPERATOR?/////////////////////////////////////////////////////////////////////////////////////
+//3.1﻿
+// Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+
+// Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
+
+const bookAuthors = [...books[0].author,...books[1].author]
+// 3.2﻿
+// Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+const spellWord = function (simpleString) {
+  console.log(...simpleString);
+  
+}
+spellWord('Word')
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//REST PATTERN AND PARAMETERS
+
+// 4.1﻿
+// Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
+
+const [mainKeyword, ...rest] = [books[0].keywords]
+
+//4.2﻿
+// Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
+
+const {publisher: bookPublisher, ...restOfTheBook} = [books[1]]
+
+// 4.3﻿
+// Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+
+function printBookAuthorsCount(title,...authors) {
+
+  console.log(`The book " ${title} has ${authors.length} authors`);
+  
+}
--- PROJECTS/09-Data-Structures-Operators/starter/script.js
+++ PROJECTS/09-Data-Structures-Operators/starter/script.js
@@ -27,47 +27,154 @@ const restaurant = {
 
   orderDeliverly: function({starterIndex =1,mainIndex = 0,time = '20:00',adress}){
     console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${ this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`);
-  }
+  },
+  //spreadoperator method
+  orderPasta: function(ing1,ing2,ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  //Rest PAttern and Parameters
+   orderPizza: function (mainIngredient, ...otherIngredients) {
+
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+   }
 };
 
-restaurant.orderDeliverly({
-  time:'22:30',
-  adress:'Via del Sole,21',
-  mainIndex:2,
-  starterIndex:2
-})
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-restaurant.orderDeliverly({
-  adress:'Via del Sole,21',
-  starterIndex:1
-})
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Rest Pattern and Parameters
+// 1) Destructuring
+//SPREAD, because on RIGHT side of  = 
 
-//Object Destructuring
+// const arr = [1,2, ...[3,4]] //El spreadoperator se usa del lado derecho para desempacar 
+
+// //REST,because on LEFT side of = 
+// const [a,b, ...others ] = [1,2,3,4,5]
+// console.log(a,b,others); // restpattern los collecta en otro arrayaparte
+
+// const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu] 
+// console.log(pizza,risotto,otherFood);
+
+// //Objects
+// const {sat, ...weekdays} = restaurant.openingHours //solo almaceno lo que hay aparte  de lo de saturday, fri and thursday
+// console.log(weekdays);
+
+// //2) Functions
+// const add = function ( ...numeros) {
+// let sum = 0  
+// for (let i = 0; i < numeros.length; i++) {
+//    sum+=numeros[i] 
+  
+// }
+// console.log(sum);
+// }
+// add(2,3)
+// add(5,3,7,2)
+// add(8,2,4,8,4,1)
+
+// const x = [23,5,7];
+
+// add(...x)
+
+
+// restaurant.orderPizza('mushrooms','Onions','Olives','Spinach')
+// restaurant.orderPizza('mushrooms')
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Spread Operator
+//bad Example, but same as below
+// const arr = [7,8,9]
+// const badNewarr = [1,2, arr[0],arr[1],arr[2]]
+// console.log(badNewarr);
+// //SPREAD OPERATOR ...arr
+// const newArr = [1,2, ...arr]
+// console.log(newArr);
+// //This is
+// console.log(...newArr);
+// //The same as 
+// console.log(1,2,7,8,9);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci']
+// console.log(newMenu);
+// //copy Array
+
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// //Join 2 arrays or more
+// const menu = [...restaurant.mainMenu,...restaurant.starterMenu]
+
+
+// //Iterables: arrays,strings,maps,sets. NOT objects
+// const str = 'Jonas';
+// const letters = [...str, '', ' S.']
+// console.log(letters);
+// console.log(...str);
+// console.log('j','o','n'); //etc
+// //console.log(`${...str}`); //DOESN't WORK
+// //IMPORTANT multiple values separated by a comma, are usually only expected when we pass arguments into a function or when we build a new array
+
+// //Real world Example
+// const ingredients = [
+//   // prompt('Let\'s make pasta! Ingredient 1?'), prompt('Let\'s make pasta! Ingredient 2?'),prompt('Let\'s make pasta! Ingredient 3?')
+// ]
+// console.log(ingredients);
+// // restaurant.orderPasta(ingredients[0],ingredients[1],ingredients[2]) //OLD SYNTAX
+// restaurant.orderPasta(...ingredients) //mordern ES6 SYNTAx
+
+
+// //Example
+// const newRestaurant = {foundedIn:1998, ...restaurant,founder:'Guiseppe'}
+// console.log(newRestaurant);
+
+
+// const restaurantCopy = {...restaurant}
+// restaurantCopy.name = 'Ristorante Roma'
+// console.log(restaurantCopy.name)
+// console.log(restaurant.name);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Destructuring Objects
+
+// restaurant.orderDeliverly({
+//   time:'22:30',
+//   adress:'Via del Sole,21',
+//   mainIndex:2,
+//   starterIndex:2
+// })
+
+// restaurant.orderDeliverly({
+//   adress:'Via del Sole,21',
+//   starterIndex:1
+// })
+
+
 //en los objetos no importa el orden como en los arrays
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 //Se le pueden colocar o definir otro nombre  alas variables al destructurar objetos
-const {
-  name: restaurntName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurntName, hours, tags);
+// const {
+//   name: restaurntName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurntName, hours, tags);
 //Default Values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
 
 //Mutating Variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-  ({ a, b } = obj);
-console.log(a, b);
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+//   ({ a, b } = obj);
+// console.log(a, b);
 
 
 //Nested Objects
-const {fri:{open:o,close:c}}= openingHours
-console.log(o,c);
+// const {fri:{open:o,close:c}}= openingHours
+// console.log(o,c);
 
 //Destructuring Arrays
 // const arr = [2, 3, 4];
