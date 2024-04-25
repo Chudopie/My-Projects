3c6b34eed07b02fec03f00a4f2dafe33a11c44b9
Eduardo Gonzalez <lalolu94@hotmail.com>
2024-04-25 00:33:46 -0600

    SET

--- PROJECTS/09-Data-Structures-Operators/starter/assignments.js
+++ PROJECTS/09-Data-Structures-Operators/starter/assignments.js
@@ -1,5 +1,5 @@
 const books = [
-  {
+  {//0
     title: 'Algorithms',
     author: ['Robert Sedgewick', 'Kevin Wayne'],
     publisher: 'Addison-Wesley Professional',
@@ -32,7 +32,7 @@ const books = [
     },
     highlighted: true,
   },
-  {
+  {//1
     title: 'Structure and Interpretation of Computer Programs',
     author: [
       'Harold Abelson',
@@ -66,7 +66,7 @@ const books = [
     },
     highlighted: true,
   },
-  {
+  {//2
     title: "Computer Systems: A Programmer's Perspective",
     author: ['Randal E. Bryant', "David Richard O'Hallaron"],
     publisher: 'Prentice Hall',
@@ -97,7 +97,7 @@ const books = [
     },
     highlighted: true,
   },
-  {
+  {//3
     title: 'Operating System Concepts',
     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
     publisher: 'John Wiley & Sons',
@@ -128,7 +128,7 @@ const books = [
       },
     },
   },
-  {
+  {//4
     title: 'Engineering Mathematics',
     author: ['K.A. Stroud', 'Dexter J. Booth'],
     publisher: 'Palgrave',
@@ -152,7 +152,7 @@ const books = [
     },
     highlighted: true,
   },
-  {
+  {//5
     title: 'The Personal MBA: Master the Art of Business',
     author: 'Josh Kaufman',
     publisher: 'Portfolio',
@@ -172,7 +172,7 @@ const books = [
       },
     },
   },
-  {
+  {//6
     title: 'Crafting Interpreters',
     author: 'Robert Nystrom',
     publisher: 'Genever Benning',
@@ -199,7 +199,7 @@ const books = [
       },
     },
   },
-  {
+  {//7
     title: 'Deep Work: Rules for Focused Success in a Distracted World',
     author: 'Cal Newport',
     publisher: 'Grand Central Publishing',
@@ -226,48 +226,48 @@ const books = [
 //Destructuring Arrays
 // 1.1﻿
 // Destructure the books array into two variables called firstBook and secondBook.
-const [firstBook, secondBook] = books;
+// const [firstBook, secondBook] = books;
 
 
 // 1.2﻿
 // Destructure the books array into a variable called thirdBook. You must skip the first two books.
-const [ , , thirdBook] = books
+// const [ , , thirdBook] = books
 
 
 // 1.3﻿
 // Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
 
-const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
-[[,ratings],[,ratingsCount]] = ratings
+// const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+// // [[,ratings],[,ratingsCount]] = ratings
 // 1.4﻿
 // Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
 
 //DESTRUCTURING OBJECTS
 
-const ratingStars = [63405, 1808];
-[fiveStarRatings,oneStarRatings,threeStarRatings=0] = ratingStars
+// const ratingStars = [63405, 1808];
+// [fiveStarRatings,oneStarRatings,threeStarRatings=0] = ratingStars
 //Destructuring Objects
 // 2.1﻿
 // Destructure the first book object from the books array into variables called title, author and ISBN.
-const {title,author,ISBN} = books[0]
+// const {title,author,ISBN} = books[0]
 
 // 2.2﻿
 // Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
 
-const {keywords:tags} = books[0]
+// const {keywords:tags} = books[0]
 
 // 2.3﻿
 // The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
 
-const {language, programmingLanguage ='unknown'} = books[6]
+// const {language, programmingLanguage ='unknown'} = books[6]
 
 // 2.4﻿
 // Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
 
-let bookTitle = 'unknown';
-let bookAuthor = 'unknown';
+// let bookTitle = 'unknown';
+// let bookAuthor = 'unknown';
 
-({title: bookTitle, author:bookAuthor}) = books[0]
+// ({title: bookTitle, author:bookAuthor}) = books[0]
 
 // 2.5﻿
 // Each book object has a deeply nested rating property as illustrated below:
@@ -289,16 +289,16 @@ let bookAuthor = 'unknown';
 
 // Please do most of the work on the left side of the assignment operator: const ... = books[0];
 
-({thirdParty:{goodreads:{rating:bookRating}}}) = books[0]
+// ({thirdParty:{goodreads:{rating:bookRating}}}) = books[0]
 
 // 2.6﻿
 // Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
 
 // If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
 
-function printBookInfo (title,author,year = 'year unknown') {
-  console.log(`The title is ${title} by ${author}, ${year}`);
-}
+// function printBookInfo (title,author,year = 'year unknown') {
+//   console.log(`The title is ${title} by ${author}, ${year}`);
+//}
 //THE SPREAD OPERATOR
 ///////////////THE SPREAD OPERATOR?/////////////////////////////////////////////////////////////////////////////////////
 //3.1﻿
@@ -306,32 +306,196 @@ function printBookInfo (title,author,year = 'year unknown') {
 
 // Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
 
-const bookAuthors = [...books[0].author,...books[1].author]
+//const bookAuthors = [...books[0].author,...books[1].author]
 // 3.2﻿
 // Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
-const spellWord = function (simpleString) {
-  console.log(...simpleString);
+// const spellWord = function (simpleString) {
+//   console.log(...simpleString);
   
-}
-spellWord('Word')
+// }
+// spellWord('Word')
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //REST PATTERN AND PARAMETERS
 
 // 4.1﻿
 // Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
 
-const [mainKeyword, ...rest] = [books[0].keywords]
+// const [mainKeyword, ...rest] = [books[0].keywords]
 
 //4.2﻿
 // Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
 
-const {publisher: bookPublisher, ...restOfTheBook} = [books[1]]
+// const {publisher: bookPublisher, ...restOfTheBook} = [books[1]]
 
 // 4.3﻿
 // Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
 
-function printBookAuthorsCount(title,...authors) {
+// function printBookAuthorsCount(title,...authors) {
 
-  console.log(`The book " ${title} has ${authors.length} authors`);
+//   console.log(`The book " ${title} has ${authors.length} authors`);
+  
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Short Circuiting (&& AND ||)
+// 5.1﻿
+// Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
+
+// {
+//   title: 'Algorithms',
+//   author: ['Robert Sedgewick', 'Kevin Wayne'],
+//   ...
+//   programmingLanguage: 'Java',     // <-- HERE
+// }
+// Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
+
+// Use short-circuiting.
+
+// const hasExamplesInJava = function(bookObject) {
+// const guests1 = bookObject.programmingLanguage === 'Java'|| 'No data Available'
+ 
+// console.log(guests1);
+  
+// }
+// hasExamplesInJava(books[0])
+
+//5.2﻿
+// Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+
+
+
+// for (let i = 0; i < books.length; i++) {
+
+//   console.log(`${books[i].title}:  ${books[i].onlineContent && " Provides online content" || " Do not provides online content"}`);
+// }
+
+// 6.1﻿
+// There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+
+// for (let i = 0; i < books.length; i++) {
+//   books[i].onlineContent ??  console.log(`${books[i].title  }:  Provides no data about its online content `);
+// }
+//8.1﻿
+// Use the for-of loop to loop over the books array and sum the pages of all books. Use the pageSum variable below, and the pages property of the book objects.\
+//  let pageSum = 0
+
+// for ( let book of books ) {
+//  pageSum+= book.pages
+//     console.log(book.pages);
+// }
+
+
+// for (const [iterator,element] of books.entries()) {
+//   console.log(iterator,element);
+// }
+// 8.2﻿
+// Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
+
+// Remember that each book object has the author property, which can be a string (if there is only a single author) or an array (if there are multiple authors). You may need to use the typeof operator. You can also use multiple loops if needed. The allAuthors array should have just one level (no nested arrays).
+let allAuthors = [];
+for (let book of books) {
+   if (typeof book.author === 'string') {
+      allAuthors.push(book.author)
+  
+   }else{
+    for (const author of book.author) {
+      allAuthors.push(author)
+    }
+   }
+}
+
+
+
+
+
+// 8.3﻿
+// Use the for-of loop together with Array's entries() method to log each author from allAuthors to the console together with its index. Make the index start from 1, instead of 0.
+
+//for (const [it,el] of allAuthors.entries()) {
+  //console.log(it+1,el);
+  //  console.log(`${it+1}: ${el}`);
+  //  console.log(typeof it, typeof el);
+//}
+
+
+// 9.1﻿
+// Below is the bookData array that contains other arrays. Each inner array consists of the property name (first element), and the value (second element). For example, in ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the property name, and 'Computer Networking: A Top-Down Approach' is meant to be the value assigned to that property name.
+
+// Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already.
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]:bookData[1][1],
+  [bookData[2][0]]:bookData[2][1],
+};
+
+// 9.2﻿
+// Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way.
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages
+}
+
+// 10.1﻿
+// Write a function called getFirstKeyword that takes the book object as an argument. This function should return the first keyword from the book's keywords property (array) or undefined (if the keywords property doesn't exist). It shouldn't throw an error. Use optional chaining for that.
+function getFirstKeyword(book) {
+  console.log(book.keywords?.[0]);
+}
+getFirstKeyword(books[0])
+
+
+// 11.1﻿
+// Below is the entries variable that stores an empty array. Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property (array) of the first book object from the books array. For each key, push a new array that contains that key to the entries array.
+
+// In the end, the entries array should be filled with arrays containing keys:
+[['rating'], ['ratingsCount'], ['reviewsCount'], ['fiveStartRatingCount'], ['oneStartRatingCount']]
+
+
+
+const entries = [];
+for (const key of object.keys(books[0].thirdParty.goodreads)) {
+  entries.push([key])
   
 }
+
+// 12.1﻿
+// Below is the allKeywords variable, which stores an empty array. Loop over the books array, and fill the allKeywords array with the keywords coming from the keywords property of each book object. The allKeywords array should have just one level (no nested arrays).
+
+// Use whatever loop and methods you want. You can also use the spread syntax. In the end, the allKeywords array should look more or less like this: ['computer science', 'programming', 'algorithms', 'data structures', ...].
+
+const allKeywords = [];
+for (const book of books) {
+  allKeywords.push(...book.keywords)
+}
+// 12.2﻿
+// The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable.
+
+const uniqueKeywords =new Set(allKeywords)
+
+// 12.3﻿
+// Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'.
+
+uniqueKeywords.add('coding','science')
+
+// 12.4﻿
+// Delete 'business' from the uniqueKeywords set.
+
+uniqueKeywords.delete('business')
+
+// 12.5﻿
+// Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.
+
+const uniqueKeywordsArr = [...uniqueKeywords]
+
+// 12.6﻿
+// Delete all items from the uniqueKeywords set.
+uniqueKeywords.clear()
--- PROJECTS/09-Data-Structures-Operators/starter/script.js
+++ PROJECTS/09-Data-Structures-Operators/starter/script.js
@@ -1,35 +1,41 @@
 'use strict';
 
+const weekdays = ['mond','tue','wed','thur','fri','sat','sun']
+
+const openingHours= {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+}
+
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  //ES6 Enhanced object literals
+  openingHours,
+  
+  order (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDeliverly: function({starterIndex =1,mainIndex = 0,time = '20:00',adress}){
+  orderDeliverly({starterIndex =1,mainIndex = 0,time = '20:00',adress}){
     console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${ this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`);
   },
   //spreadoperator method
-  orderPasta: function(ing1,ing2,ing3) {
+  orderPasta(ing1,ing2,ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
@@ -41,27 +47,288 @@ const restaurant = {
    }
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// \
+//SETS AND  MAPS
+
+
+//SET      ALL duplicates are gone
+const ordersSet = new Set(['Pasta','Pizza','Pizza','Risotto','Pasta','Pizza'])
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread')); //Similar as includes
+ordersSet.add('Garlic Bread')
+ordersSet.delete('Risotto')
+//TO ERASE EVERYTHING
+// ordersSet.clear()   
+console.log(ordersSet);//there are no index, u can't get values of a set
+
+for(const order of ordersSet){
+  console.log(order);
+}
+
+//Example 
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager','Chef','Waiter']
+
+const staffUnique = [...new Set(staff)]
+console.log(staffUnique);
+
+
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager','Chef','Waiter']).size);
+
+console.log(new Set('jonassschmedtmann').size);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//Looping Objects: Object Keys, Values, and Entries
+//Property NAMES
+// const properties = Object.keys(openingHours)
+// console.log(properties);
+
+// let openStr =`We are open on ${properties.length} days: `
+// for (const day of Object.keys(openingHours)) {
+//   openStr+= ` ${day},  `
+// }
+// console.log(openStr);
+
+// //Property VALUES
+// const values = Object.values(openingHours)
+// console.log(values);
+// //Entries Object
+// const entries = Object.entries(openingHours)
+// console.log(entries);
+// // [key,value]
+// for (const [day,{open,close}] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Optional Chaining
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open)
+// }
+// //ES2020 with optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
+
+// //Example
+// const days = ['mond','tue','wed','thur','fri','sat','sun']
+//   for (const day of days) {
+   
+//   const open = restaurant.openingHours[day]?.open ?? 'closed'
+//     console.log(`On ${day}, we open at ${open}`);
+// }
+// //Optional Chaining on Methods
+// console.log(restaurant.order?.(0,1)?? 'Method does not exist'); //this exists
+// console.log(restaurant.orderRisotto?.(0,1)?? 'Method does not exist');//Orderrisotto does not exist
+
+// //Optional Chaining on Arrays
+// const users = [
+//   {name:'Jonas',email:'hello@jonas.io'}
+// ]
+// console.log(users[0]?.name ?? 'User array empty');//users[0] exists
+// console.log(users[2]?.name ?? 'User array empty');//users[2] does not exissts
+
+// //---V     this is the same but longer
+// if (users.length>0) {
+//   console.log(users[0].name);
+// }else{
+//   console.log('users array empty');
+// }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Enhanced Object Literals
+//ES6 Enhanced object literals
+//openingHours
+//hours
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Looping Arrays: The for-of Loop
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
+// for (const item of menu){
+//   console.log(item)}
+
+  
+// for (const [i, el] of menu.entries()) {
+//     console.log(`${i + 1}: ${el}, `);
+// }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Local Asignment Operators
+
+// const rest1 = {
+//   name:'Capri',
+//   numGuests:0,
+// }
+// const rest2 = {
+//   name:'La Piazza',
+//   owner:'Giovanni Rossi',
+// }
+
+//OR assignment Operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+//Same as above
+// rest1.numGuests ||= 10
+// rest2.numGuests ||= 10
+// rest1.owner ||= 'Bonifio Bonatelli'
+
+//The  Nullish Assignment operator (null or undefined)
+//rest1.numGuests ??= 10
+//rest2.numGuests ??= 10
+
+
+//AND assignment operator
+//rest1.owner = rest1.owner &&'<ANONYMUS>'
+// rest2.owner &&= '<ANONYMUS>'
+//rest2.owner = rest2.owner &&'<ANONYMUS>'
+// rest2.owner &&= '<ANONYMUS>'
+// console.log(rest1);
+// console.log(rest2);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// //1
+// const [players1,players2] = game.players
+// console.log(players1,players2);
+// //2
+// const[gk, ...fieldPlayers] = players1
+// console.log(gk,fieldPlayers);
+// //3
+// const allPlayers = [...players1, ...players2]
+// console.log(allPlayers);
+// //4
+// const players1Final = [...players1,'Thiago','Coutinho' ,'Perisic']
+// console.log(players1Final);
+// //5
+// // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const {odds:{team1,x:draw,team2}} = game
+// console.log(team1,draw,team2);
+// //6 . Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// function printGoals(...playerNames) {
+  
+//     console.log(playerNames);
+//     console.log(`${playerNames.length} Goals Where Scored`);
+// }
+
+// printGoals( 'Davies', 'Muller', 'Lewandowski','Kimmich')
+// printGoals(...game.scored)
+// //7
+// team1>team2 && console.log(`team1 is more likely to win with ${team1}`);
+// team2>team1 && console.log(`team2 is more likely to win with ${team2}`);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//NULLISH COALESCING OPERATOR (??)
+
+// restaurant.numGuest = 0;
+// const guests = restaurant.numGuest || 10;
+// console.log(guests);
+
+
+///NULISH values are null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuest ?? 10;
+// console.log(guestCorrect);
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //SHORT CIRCUITING
 
 //Use ANY data type, return ANY data type
 //short-circuiting 
-
+// console.log('-------------OR----------------');
 //En un OR si el primer valor es True, el resultado sera el primer valor//
-console.log(3 || 'Jonas'); //Resultado 3 
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
+// console.log(3 || 'Jonas'); //Resultado 3 
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
 
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
 // restaurant.numGuests=23
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10
-console.log(guests1);
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10
+// console.log(guests1);
 
-const guests2 = restaurant.numGuests || 10
-console.log(guests2);
+// const guests2 = restaurant.numGuests || 10
+// console.log(guests2);
+// console.log('-------------AND-------------------');
 
+///////////////ANBD
+// console.log(0 && 'Jonas');
+// console.log(7&&'Jonas');
+// console.log('Hello'&&23&&null && 'Jonas');
+
+//Practical Example
+//With an IF
+// if(restaurant.orderPizza){
+//   restaurant.orderPizza('Mushrooms', ' Spinach')
+// }
+//With Short Circuiting AND
+// restaurant.orderPizza && restaurant.orderPizza('Mushrooms','Spinach')
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Rest Pattern and Parameters
