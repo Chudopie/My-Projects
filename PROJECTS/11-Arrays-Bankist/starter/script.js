5538dde953cebb734f98ddc5d00c6f467ce4c7f8
Eduardo Gonzalez <lalolu94@hotmail.com>
2024-07-08 10:27:58 -0600

    foreachhalftorrtas

--- PROJECTS/11-Arrays-Bankist/starter/script.js
+++ PROJECTS/11-Arrays-Bankist/starter/script.js
@@ -71,42 +71,70 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-let arr = ['a','b','c','d','e']
+// let arr = ['a','b','c','d','e']
 
-console.log( arr.slice(2));
-console.log(arr.slice(2,4));
-console.log(arr.slice(-2));
-console.log(arr.slice(1,-1));
-//Those are the sames
-console.log(arr.slice());
-console.log([...arr]);
+// console.log( arr.slice(2));
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(1,-1));
+// //Those are the sames
+// console.log(arr.slice());
+// console.log([...arr]);
 
-//SPLICE
-// console.log(arr.splice(2));
-//it changes, the splice, just deleted the elements they select, and saves the others in the main array
+// //SPLICE
+// // console.log(arr.splice(2));
+// //it changes, the splice, just deleted the elements they select, and saves the others in the main array
 
-arr.splice(-1);
-console.log(arr);
-arr.splice(1,2);
-console.log(arr);
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1,2);
+// console.log(arr);
 
-arr = ['a','b','c','d','e']
+// arr = ['a','b','c','d','e']
 
-const arr2 = ['j','i','h','g','f']
-console.log(arr2);
-console.log(arr2.reverse()); //this mutate the original array //expected output :  ['f', 'g', 'h', 'i', 'j']
-console.log(arr2);
+// const arr2 = ['j','i','h','g','f']
+// console.log(arr2);
+// console.log(arr2.reverse()); //this mutate the original array //expected output :  ['f', 'g', 'h', 'i', 'j']
+// console.log(arr2);
 
-//CONCAT
+// //CONCAT
 
-const letters = arr.concat(arr2);//doesnt mutate the original array
-console.log(letters);
-console.log([...arr, ...arr2]);//exact result, and this not mutate
+// const letters = arr.concat(arr2);//doesnt mutate the original array
+// console.log(letters);
+// console.log([...arr, ...arr2]);//exact result, and this not mutate
 
 
-//JOIN
+// //JOIN
 
-console.log(letters.join('-')); //expected output a-b-c-d-e-f-g-h-i-j
+// console.log(letters.join('-')); //expected output a-b-c-d-e-f-g-h-i-j
+
+// const arr = [23,11,64]
+// console.log(arr[0]);
+
+// console.log(arr.at(0));
+// //getting the last element
+// console.log(arr[arr.length-1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1)); //or /-2
+// //also with strings
+// console.log('jonas'.at(-0));
+// console.log('jonas'.at(-1));
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements){
+  if(movement>0)
+    {
+      console.log(`You deposited ${movement}`);
+    }else{
+      console.log(`You withdraw ${Math.abs(movement)}`); //Math.abs to remove the sign
+    }
+}
+
+movements.forEach(function () {
+  
+})
